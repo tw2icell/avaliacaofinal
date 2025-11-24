1c99bdd2c2e1da5eaa7d23869f54dc2bebfe1d64
tw2icell <laura.tavares@aluno.institutocapibaribe.com.br>
2025-11-24 16:30:21 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+import view.Agendaview;
+
+public class Main {
+    public static void main(String[] args) {
+        new.Agendaview().start();
+    }
+
+    private void start() {
+      
+    }
+}
